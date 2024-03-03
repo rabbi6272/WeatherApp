01312ea2a7f12586d5430ca630048c10b54c2fea
Rabbi <140103650+rabbi6272@users.noreply.github.com>
2024-03-03 21:32:49 +0600

    released v2

--- app.js
+++ app.js
@@ -10,22 +10,15 @@ app.set('view engine', 'ejs');
 
 app.post("/", (req, res) => {
   const city = req.body.cityName;
-  console.log(city);
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bbe7d4cf002908df2b1cf82e3d4a947e&units=metric`;
   http.get(url, (response) => {
     response.on("data", (data) => {
       if (response.statusCode === 200) {
         const weatherData = JSON.parse(data);
-        console.log(weatherData);
         var temp = weatherData.main.temp;
         var description = weatherData.weather[0].description;
         var icon = weatherData.weather[0].icon;
         var imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-
-        // res.write("<h1>It is currently " + temp + "&#8451 in" + city + "</h1>");
-        // res.write("<h2>The weather is " + description + "</h2>");
-        // res.write("<img src=" + imgURL + ">");
-        // res.send();
         res.render("responce", {temp: temp, city: city, description: description, imgURL: imgURL});
       } else {
         res.sendFile(__dirname + "/public/failure.html");

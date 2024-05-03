5385b5e7e9ab61f5318ba94d9ef12e4752729c54
Rabbi <140103650+rabbi6272@users.noreply.github.com>
2024-05-03 16:26:47 +0600

    changed ui and added more features

--- app.js
+++ app.js
@@ -13,16 +13,33 @@ app.get("/", (req, res) => {
 });
 app.post("/", (req, res) => {
   const city = req.body.cityName;
+  // let url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=5ChYAeDhhMUMoVudNjFxL5euJ8RjS8Ew&q=${city}`;
+  // http.get(url, (response) => {
+  //     response.on("data", (data) => {
+  //       if (response.statusCode === 200) {
+  //         const key = JSON.parse(data)[0].Key;
+  //         console.log(key);
+  //       }
+  //     });
+  //   });
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bbe7d4cf002908df2b1cf82e3d4a947e&units=metric`;
   http.get(url, (response) => {
     response.on("data", (data) => {
       if (response.statusCode === 200) {
         const weatherData = JSON.parse(data);
-        var temp = Math.trunc(weatherData.main.temp);
-        var description = weatherData.weather[0].description;
-        var icon = weatherData.weather[0].icon;
-        var imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-        res.render("responce", {temp: temp, city: city, description: description, imgURL: imgURL, country: weatherData.sys.country});
+        res.render("responce", {
+          temp: Math.trunc(weatherData.main.temp),
+          feelsLike: weatherData.main.feels_like,
+          tempMin: weatherData.main.temp_min,
+          tempMax: weatherData.main.temp_max,
+          humidity: weatherData.main.humidity,
+          pressure: weatherData.main.pressure,
+          wind: weatherData.wind.speed,
+          description: weatherData.weather[0].description, 
+          imgURL: "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png", 
+          city: weatherData.name, 
+          country: weatherData.sys.country,
+        });
       } else {
         res.render("failure");
       }
@@ -37,6 +54,9 @@ app.post("/again", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000 , () => {
   console.log("Listening on port 3000");
 });
+
+//5ChYAeDhhMUMoVudNjFxL5euJ8RjS8Ew
+//0.Key
---
+++ public/script.js
@@ -0,0 +1,7 @@
+
+gsap.to(".img",{
+    duration: 3,
+    y: 10,
+    repeat: -1,
+    yoyo: true
+});
